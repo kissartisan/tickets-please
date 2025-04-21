6c9c105dcc7d3cd8548a2346678ef35f502e80de
kissartisan <kissartisan@gmail.com>
2025-04-22 02:38:04 +0800

    Establish API versioning

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,6 @@ Route::get('tickets', function () {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(base_path('routes/api_v1.php'));
+Route::prefix('v2')->group(base_path('routes/api_v2.php'));
