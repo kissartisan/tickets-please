772556044a6824b2396965d30c54cafd36b05f50
kissartisan <kissartisan@gmail.com>
2025-05-24 21:00:35 +0800

    Add logout functionality

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginUserRequest;
 use App\Models\User;
 use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
@@ -25,6 +26,13 @@ class AuthController extends Controller
         return $this->ok('Authenticated', [
             'token' => $user->createToken('API Token for ' . $user->email)->plainTextToken,
         ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('');
     }
 
     public function register()
