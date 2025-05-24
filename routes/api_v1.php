eb5fc1478db90c5751ddea1efa3ecb2fe61b100c
kissartisan <kissartisan@gmail.com>
2025-05-24 19:05:12 +0800

    Authentication using tokens

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 
 
